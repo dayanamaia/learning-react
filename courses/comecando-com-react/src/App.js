1e3e7caa98e2bd041f8ecdf997581ad3863c3f97
Dayana Maia <dayrmaia@gmail.com>
2020-11-02 18:47:04 -0300

    feat: adding function component Github

--- courses/comecando-com-react/src/App.js
+++ courses/comecando-com-react/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById('app'));
+import Github from './components/Github';
+
+ReactDom.render(<Github />, document.getElementById('app'));
---
+++ courses/comecando-com-react/src/components/Github.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Github = () => {
+    return(
+        <>
+        <h2>component Github</h2>
+        </>
+    )
+}
+
+export default Github;
